3135cd6de459ada17d0faab0ae216f715644c90d
cindyfadila271784 <cindyfadila485@gmail.com>
2024-10-24 15:08:46 +0700

    selesai

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-function runner() {
+//function runner() {
 
   // TULIS SCRIPT KAMU DI SINI
 
+  let tinggiBadan = ["150", "155", "148", "165", "170", "168", "166"]
 
+  let namaAtlet = ["rizki","ambar", "rada", "tika", "malik", "nanang","azizah"]
+  for (let i = 0; i < 7; i++) {
+    
+    console.log(`Atlet ${namaAtlet[i]} tinggi badanya ${tinggiBadan [i]} cm`)
+  }
 
 
 
 
   /// JANGAN UBAH SELAINNYA
-}
+//}
 
-module.exports = runner
+//module.exports = runner
